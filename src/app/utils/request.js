73b40a997c4bb679f85ba7bfd41d298a04f14574
Jorge Yau <codenameyau@gmail.com>
2020-02-26 16:20:32 -0500

    Update fetch utils

--- src/app/utils/request.js
+++ src/app/utils/request.js
@@ -12,11 +12,40 @@ export const defaultOptions = {
  * @returns  {Promise}
  */
 export const request = (url, options = defaultOptions) => {
-  if (options.body && options.headers['Content-Type'] === 'application/json') {
-    options.body = JSON.stringify(options.body);
+  const httpOptions = {
+    ...options,
+  };
+
+  if (options.body) {
+    httpOptions.body = JSON.stringify(options.body);
   }
 
-  return fetch(url, options);
+  return fetch(url, httpOptions).then(res => {
+    if (res.status === 204) {
+      return {
+        status: 204,
+      };
+    }
+
+    return res
+      .json()
+      .catch(error => {
+        console.error(error);
+      })
+      .then(json => {
+        if (json instanceof Array || Array.isArray(json)) {
+          return {
+            body: json,
+            status: res.status,
+          };
+        }
+
+        return {
+          ...json,
+          status: res.status,
+        };
+      });
+  });
 };
 
 export default request;
